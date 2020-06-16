c490052a13f7c8117ceaf9c8ba3529046da61b88
Megan Gomes <megan.gomes25@gmail.com>
2020-06-16 18:07:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,22 @@ class Triangle
   
   def kind 
     sides = [side_1, side_2, side_3]
+    ineq = [side_1 + side_2 > side_3, side_1 + side_3 > side_2, side_2 + side_3 > side_1]
+    
+    if sides.min <= 0 || ineq.include?(false)
+      raise TriangleError 
+    end
+    
+    if side_1 == side_2 && side_2 == side_3
+      :equilateral
+    elsif side_1 != side_2 && side_1 != side_3 && side_2 != side_3
+      :scalene
+    elsif (side_1 == side_2 && side_2 != side_3) || (side_2 == side_3 && side_3 != side_1) || (side_1 == side_3 && side_3 != side_2)
+      :isosceles
+    end
+  end
+  
+  
     
     
   
